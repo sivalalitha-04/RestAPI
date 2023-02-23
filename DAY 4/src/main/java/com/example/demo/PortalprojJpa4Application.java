d530e96e9556a1e2e307fa15cd780c637b7ab7cc
SIVALALITHA KANAGAVEL <sivalalithakanagavel04@gmail.com>
2023-02-23 22:01:38 +0530

    put,delete restApi

---
+++ DAY 4/src/main/java/com/example/demo/PortalprojJpa4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalprojJpa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalprojJpa4Application.class, args);
+	}
+
+}
