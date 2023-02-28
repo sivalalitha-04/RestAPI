39db559835148e7af67c2fbba9d7bf153a481507
SIVALALITHA KANAGAVEL <sivalalithakanagavel04@gmail.com>
2023-02-28 12:28:04 +0530

    day 7 queryop

---
+++ DAY 7/src/main/java/com/example/demo/PortprojJpql7Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortprojJpql7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortprojJpql7Application.class, args);
+	}
+
+}
